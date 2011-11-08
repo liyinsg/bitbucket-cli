128ea2c6bc831c048a53d180512312f749f9f9aa
Zhehao Mao <zhehao.mao@gmail.com>
2011-11-08 14:13:41 -0500

    update setup script

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name = 'bitbucket-cli',
-	version = '0.1.0',
+	version = '0.1.1',
 	author = 'Zhehao Mao',
 	author_email = 'zhehao.mao@gmail.com',
 	description = 'BitBucket command line interface',
