69ef7f620732bf998b65e32450859b55802004c6
Zhehao Mao <zhehao.mao@gmail.com>
2012-08-02 17:31:41 -0400

    bump patch number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name = 'bitbucket-cli',
-	version = '0.2.2',
+	version = '0.2.3',
 	author = 'Zhehao Mao',
 	author_email = 'zhehao.mao@gmail.com',
 	description = 'BitBucket command line interface',
