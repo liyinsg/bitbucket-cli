b7a29df81787ec2465b7b368fb9d2e0b4e5d560d
jason <jason@zzq.org>
2015-02-19 11:59:01 -0700

    Working open pull request

--- bitbucket/repositories.py
+++ bitbucket/repositories.py
@@ -18,10 +18,8 @@ def _json_or_error(r):
     # XXX: alternatively, we could pass in the expected error.
     if r.status_code not in range(200, 300):
         r.raise_for_status()
-    try:
-        return json.loads(r.content)
-    except:
-        pass
+
+    return json.loads(r.content)
 
 
 def get_user_repos(username, password=''):
@@ -58,7 +56,8 @@ def get_branches(ownername, repo_slug, username, password=''):
 
 def open_pull(username, password, ownername, repo_slug, source='',
               destination='master', title='',
-              description='This request was automatically generated.'):
+              description='This request was automatically generated.',
+              close_source_branch=True, reviewers=''):
     ''' Opens a pull request against the current repository. '''
     name = ownername or username
     url = BASE_URL_V2 + 'repositories/{0}/{1}/pullrequests'.format(name,
@@ -68,17 +67,30 @@ def open_pull(username, password, ownername, repo_slug, source='',
         title = 'Merging {0} into {1}'.format(source, destination)
 
     payload = {'title': title,
-               'description': description,
-               'source': {'branch': {'name': source},
-                          'repository': {'full_name': full_name}},
-               'destination': {'branch': {'name': destination}},
-               'reviewers': [{'username': ''}],
-               'close_source_branch': 'false'}
+           'description': description,
+           'source': {
+               'branch': {
+                   'name': source,
+               },
+               'repository': {
+                   'full_name': full_name,
+               }
+           },
+         'destination': {
+             'branch': {
+                 'name': destination
+             }
+         },
+         # 'reviewers': [
+         #     {
+         #         'username': ''
+         # }],
+         'close_source_branch': close_source_branch
+    }
     headers = {'Content-Type': 'application/json'}
 
-    r = requests.post(url, data=payload, headers=headers,
+    r = requests.post(url, data=json.dumps(payload), headers=headers,
                       auth=(username, password))
-    print r.headers
     return _json_or_error(r)
 
 
